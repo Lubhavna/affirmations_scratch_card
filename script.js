71bf5f6ff5bbf3734927a8fa1b03d715d20c7a2f
Lubhavna <lubhavnagoel2003@gmail.com>
2025-05-12 23:13:22 +0530

    changed the APIs used and updated the key

--- script.js
+++ script.js
@@ -1,52 +1,73 @@
-const API_KEY =
-  "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";
-const API_URL = "https://openrouter.ai/api/v1/chat/completions";
-const QuotesKey = "NVpT34kvJfXcJX8WHn40CA==cDKSFmdR7O1J2Y4a";
+const quotesKey = "***REMOVED***";
+const affirmationsKey =
+  "***REMOVED***";
 const daily_quotes_text = document.querySelector(".daily-quote-text");
-const daily_quotes_author = document.querySelector(".daily-quote-author");
 const new_quote_btn = document.querySelector(".new-quote-btn");
+const affirm_text_1 = document.querySelector(".affirmation-text-1");
+const affirm_text_2 = document.querySelector(".affirmation-text-2");
+const affirm_text_3 = document.querySelector(".affirmation-text-3");
+const generate_affirmation_btn = document.querySelector(".affirm-btn");
+const zodiac_sign = document.querySelector(".zodiac-val");
 async function getRandomQuotes() {
-  try {
-    const Affirm_URL = `https://api.api-ninjas.com/v1/quotes`;
-    const data = await fetch(Affirm_URL, {
+  const response = await fetch(
+    "https://api.groq.com/openai/v1/chat/completions",
+    {
+      method: "POST",
       headers: {
-        "X-Api-Key": QuotesKey,
+        ***REMOVED***: `***REMOVED*** ${quotesKey}`,
+        "Content-Type": "application/json",
       },
-    });
-    const res = await data.json();
-    daily_quotes_text.textContent = `${res[0].quote} -`;
-    daily_quotes_author.textContent = res[0].author;
-  } catch (err) {
-    console.log(err.message);
-  }
+      body: JSON.stringify({
+        model: "llama-3.3-70b-versatile",
+        messages: [
+          {
+            role: "user",
+            content:
+              "Generate a short but powerful quote with theme of confidence",
+          },
+        ],
+      }),
+    }
+  );
+  const quote = await response.json();
+  console.log(quote.choices[0].message.content);
+  daily_quotes_text.textContent = quote.choices[0].message.content;
 }
-async function getZodiacAffirmations(zodiac) {
-  const res = await fetch(API_URL, {
-    headers: {
-      ***REMOVED***: `***REMOVED*** ${API_KEY}`,
-      "Content-Type": "application/json",
-    },
-    method: "POST",
-    body: JSON.stringify({
-      model: "qwen/qwen3-1.7b:free",
-      messages: [
-        {
-          role: "user",
-          content: `Give me 3 short affirmations for a ${zodiac}`,
-        },
-      ],
-    }),
-  });
-  const data = await res.json();
-  console.log(data);
+async function getZodiacAffirmations() {
+  const zodiac = zodiac_sign.value;
+  const response = await fetch(
+    "https://api.groq.com/openai/v1/chat/completions",
+    {
+      method: "POST",
+      headers: {
+        ***REMOVED***: `***REMOVED*** ${affirmationsKey}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        model: "llama-3.3-70b-versatile",
+        messages: [
+          {
+            role: "user",
+            content: `Generate three short but powerful affirmations for the zodiac sign ${zodiac} and add '~' after each and don't number them.`,
+          },
+        ],
+      }),
+    }
+  );
+  const affirmation = await response.json();
+  const a1 = affirmation.choices[0].message.content;
+  let affirm = a1.split("~");
+  console.log(a1);
+  affirm_text_1.textContent = affirm[0];
+  affirm_text_2.textContent = affirm[1];
+  affirm_text_3.textContent = affirm[2];
 }
-new_quote_btn.addEventListener("click", getRandomQuotes);
 function createScratchCard(cardId, color) {
   const canvas = document.getElementById(cardId);
   const ctx = canvas.getContext("2d");
   const init = () => {
     ctx.fillStyle = color;
-    ctx.fillRect(0, 0, 400, 320);
+    ctx.fillRect(0, 0, 320, 320);
   };
   let isDragging = false;
   function scratchCard(x, y) {
@@ -84,9 +105,8 @@ function createScratchCard(cardId, color) {
   canvas.addEventListener("touchend", mouseUp);
   init();
 }
-
-getZodiacAffirmations("Aries");
-getRandomQuotes();
+generate_affirmation_btn.addEventListener("click", getZodiacAffirmations);
+new_quote_btn.addEventListener("click", getRandomQuotes);
 createScratchCard("scratch-1", "#D9C4B1");
 createScratchCard("scratch-2", "#D9C4B1");
 createScratchCard("scratch-3", "#D9C4B1");
